f6d51e63424855a7b8cfcfb5d8676f3701bacf84
Ian Cristino <2224378@slu.edu.ph>
2023-09-17 19:01:04 +0800

    modified menu program and made packages and files demo ready

--- hello.go
+++ hello.go
@@ -12,58 +12,53 @@ func menu() {
 	//TODO make problems
 	//FIXME break out of loop when one of switch case is triggered and proceed to method in switch case
 
-Loop:
-	for {
-		fmt.Println("1. ")
-		fmt.Println("2. ")
-		fmt.Println("3. ")
-		fmt.Println("4. ")
-		fmt.Println("5. ")
-		fmt.Println("6. ")
-		fmt.Println("7. ")
-		fmt.Println("8. ")
-		fmt.Println("9. ")
-		fmt.Println("10. ")
-		fmt.Println("11. Exit")
-		fmt.Println("Please input a number: ")
-		fmt.Scan(&input)
+	fmt.Println("1. ")
+	fmt.Println("2. ")
+	fmt.Println("3. ")
+	fmt.Println("4. ")
+	fmt.Println("5. ")
+	fmt.Println("6. ")
+	fmt.Println("7. ")
+	fmt.Println("8. ")
+	fmt.Println("9. ")
+	fmt.Println("10. ")
+	fmt.Println("11. Exit")
+	fmt.Println("Please input a number: ")
+	fmt.Scan(&input)
 
-		switch input {
-		case 1:
-			firstProblem()
-			break Loop
-		case 2:
+	switch input {
+	case 1:
+		firstProblem()
+	case 2:
 
-		case 3:
+	case 3:
 
-		case 4:
+	case 4:
 
-		case 5:
+	case 5:
 
-		case 6:
+	case 6:
 
-		case 7:
+	case 7:
 
-		case 8:
+	case 8:
 
-		case 9:
+	case 9:
 
-		case 10:
+	case 10:
 
-		case 11:
-			fmt.Println("Thank you for using our program...")
-		default:
-			fmt.Println("Please enter a valid number from the options menu.")
-		}
-		if input == 11 {
-			break
-		}
+	case 11:
+		fmt.Println("Thank you for using our program...")
+	default:
+		fmt.Println("Please enter a valid number from the options menu.")
 	}
-
 }
 
 func firstProblem() {
 	var sagot string
-	fmt.Println("Suppose na bading si jobert, how many can he succcc? ")
+	fmt.Println("Test?")
 	fmt.Scanln(&sagot)
+
+	fmt.Scanln()
+	menu()
 }
---
+++ helloworld.go
@@ -0,0 +1,10 @@
+// defines package name
+package main
+
+//preprocessor command
+import "fmt"
+
+// main function
+func main() {
+	fmt.Println("Hello World")
+}
