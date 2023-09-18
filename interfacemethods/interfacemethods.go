abfcc04ca96bd68a46bcaf310a9d3d2799c52046
Malang <2223499@slu.edu.ph>
2023-09-18 13:19:27 +0800

    added interface and methods

---
+++ interfacemethods/interfacemethods.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Define the Shape interface with a single method: Area()
+type Shape interface {
+	Area() float64
+}
+
+// Create a struct for Circle
+type Circle struct {
+	Radius float64
+}
+
+// Implement the Area() method for Circle
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+// Create a struct for Rectangle
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+// Implement the Area() method for Rectangle
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func main() {
+	// Create instances of Circle and Rectangle
+	circle := Circle{Radius: 5.0}
+	rectangle := Rectangle{Width: 4.0, Height: 3.0}
+
+	// Calculate and display the area of the Circle
+	fmt.Printf("Circle Area: %.2f\n", circle.Area())
+
+	// Calculate and display the area of the Rectangle
+	fmt.Printf("Rectangle Area: %.2f\n", rectangle.Area())
+}
