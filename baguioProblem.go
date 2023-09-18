55f737bf38de51c54660b239a768303974a38e45
Zev Josef <2221957@slu.edu.ph>
2023-09-19 02:12:54 +0800

    Edited baguioProblem

--- baguioProblem.go
+++ baguioProblem.go
@@ -46,31 +46,15 @@ func main() {
 
 				switch locationChoice {
 				case 1:
-					// User chose Sm Baguio City
-					displaySMParkingStations()
-					var stationChoice int
-					fmt.Print("Enter your station choice: ")
-					fmt.Scanln(&stationChoice)
-
-					switch stationChoice {
-					case 1:
-						// User chose Old Parking Area
-						// Implement the reservation logic for Old Parking Area here
-						fmt.Println("Reserved a parking slot in Sm Baguio City's Old Parking Area")
-					case 2:
-						// User chose New Parking Area
-						// Implement the reservation logic for New Parking Area here
-						fmt.Println("Reserved a parking slot in Sm Baguio City's New Parking Area")
-					case 3:
-						// User chose Outside Parking Area
-						// Implement the reservation logic for Outside Parking Area here
-						fmt.Println("Reserved a parking slot in Sm Baguio City's Outside Parking Area")
-					default:
-						fmt.Println("Invalid station choice. Please select a valid option.")
+					{
 					}
-				// Add more cases for other landmarks if needed
-				default:
-					fmt.Println("Invalid location choice. Please select a valid option.")
+				case 2:
+					{
+					}
+				case 3:
+					{
+					}
+
 				}
 				var slotNumber int
 				fmt.Print("Enter the slot number to reserve: ")
@@ -118,13 +102,6 @@ func displayLocations() {
 	fmt.Println("1. Sm Baguio City")
 	fmt.Println("2. Burnham Park")
 	fmt.Println("3. Camp John Hay")
-}
-
-func displaySMParkingStations() {
-	fmt.Println("Sm Baguio City")
-	fmt.Println("1. Old Parking Area")
-	fmt.Println("2. New Parking Area")
-	fmt.Println("3. Outisde parking Area")
 }
 
 func generateRandomID() string {
